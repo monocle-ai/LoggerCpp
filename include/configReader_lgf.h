024f2160cb29a073d529e306d1bb5f721b229f8e
dasdipanjan04 <dasdipanjan04@gmail.com>
2020-03-31 00:53:11 +0200

    first basic implementation of config_reader

---
+++ include/configReader_lgf.h
@@ -0,0 +1,49 @@
+/*
+MIT License
+
+Copyright(c) 2020 Dipanjan Das
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright noticeand this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+If you contribute to this file please add your name to the contributors list below.
+
+Author		 : Dipanjan Das
+Contributors :
+*/
+#pragma once
+#ifndef LGF_CONFIGREADER_H
+#define LGF_CONFIGREADER_H
+
+#include "core_lgf.h"
+
+LGF_BEGIN
+
+class ConfigReader
+{
+	public:
+		ConfigReader(const std::shared_ptr<STRING_VIEW>& configFilePath);
+		~ConfigReader();
+		std::tuple<STRING_VIEW, STRING_VIEW> getLoggerConfiguration();
+	private:
+		const std::shared_ptr<STRING_VIEW> m_configFilePath;
+};
+
+LGF_END
+
+#endif
---
+++ src/configReader_lgf.cpp
@@ -0,0 +1,39 @@
+#include "configReader_lgf.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+namespace LGF
+{
+	ConfigReader::ConfigReader(const std::shared_ptr<STRING_VIEW>& configFilePath): m_configFilePath(configFilePath)
+	{
+	}
+
+	ConfigReader::~ConfigReader()
+	{
+	}
+
+	std::tuple<STRING_VIEW, STRING_VIEW> ConfigReader::getLoggerConfiguration()
+	{
+		std::ifstream configFile(m_configFilePath->data());
+		std::string configVar;
+		std::string configVal;
+		if (configFile.is_open())
+		{
+			std::string configLine;
+			while(std::getline(configFile, configLine))
+			{
+				auto configDelimiter = configLine.find("=");
+				configVar = configLine.substr(0, configDelimiter);
+				configVal = configLine.substr(configDelimiter + 1);
+			}
+		}
+		fmt::memory_buffer bufVar;
+		fmt::format_to(bufVar, configVar);
+		fmt::memory_buffer bufVal;
+		fmt::format_to(bufVal, configVal);
+		std::cout << "config name is: " << toStringView(bufVar) << " config val is: " << toStringView(bufVal) << std::endl;
+		return { toStringView(bufVar), toStringView(bufVal) };
+	}
+}
